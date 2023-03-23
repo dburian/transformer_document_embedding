55e463f42eaf5b5ebe6e2525e3e72aa34071eaf6
David Burian <david.burian@me.com>
2023-03-24 00:27:56 +0100

    Longformer trainer loop refactor

--- src/transformer_document_embedding/baselines/longformer.py
+++ src/transformer_document_embedding/baselines/longformer.py
@@ -1,4 +1,5 @@
 import math
+import os
 import numpy as np
 from typing import Optional, Any, Iterable, cast
 from torch.optim.lr_scheduler import LambdaLR
@@ -7,6 +8,7 @@ from transformers import (
     AutoTokenizer,
     AutoConfig,
     LongformerForSequenceClassification,
+    PreTrainedModel,
 )
 
 from transformer_document_embedding.tasks.imdb import IMDBClassification, IMDBData
@@ -14,7 +16,7 @@ from transformers.data.data_collator import DataCollatorWithPadding
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard.writer import SummaryWriter
-from torch.cuda.amp import GradScaler
+from torch.cuda.amp.grad_scaler import GradScaler
 from torcheval.metrics import Metric, MulticlassAccuracy, Mean
 from tqdm import tqdm
 
@@ -37,11 +39,12 @@ class LongformerIMBD(ExperimentalModel):
         config = AutoConfig.from_pretrained("allenai/longformer-base-4096")
         config.num_labels = 2
         config.classifier_dropout = classifier_dropout
-                
+
         self._model = cast(
             LongformerForSequenceClassification,
             LongformerForSequenceClassification.from_pretrained(
-                "allenai/longformer-base-4096", config=config,
+                "allenai/longformer-base-4096",
+                config=config,
             ),
         )
         self._tokenizer = AutoTokenizer.from_pretrained("allenai/longformer-base-4096")
@@ -58,34 +61,45 @@ class LongformerIMBD(ExperimentalModel):
             torch.cuda.is_available()
         ), f"Training {LongformerIMBD.__name__} is available only with gpu."
 
-        train = self._prepare_data(task.train)
-        val = None
+        train_data = self._prepare_data(task.train)
+        val_data = None
         if hasattr(task, "validation"):
             # TODO: Better implementation of this
             #   - property on `ExperimentalTask`
             #   - default implementation of validation returning None
-            val = self._prepare_data(task.validation)
+            val_data = self._prepare_data(task.validation)
 
         optimizer = torch.optim.Adam(self._model.parameters(), lr=3e-5)
 
-        self._model.to(torch.device("cuda"))
-        self._train(
-            train=train,
-            val=val,
+        self._model.gradient_checkpointing_enable()
+        summary_writer = (
+            SummaryWriter(os.path.join(log_dir, "train"))
+            if log_dir is not None
+            else None
+        )
+        val_summary_writer = (
+            SummaryWriter(os.path.join(log_dir, "val")) if log_dir is not None else None
+        )
+
+        train(
+            model=self._model,
+            train_data=train_data,
+            val_data=val_data,
             epochs=self._epochs,
             loss_fn=torch.nn.CrossEntropyLoss(),
             optimizer=optimizer,
             metrics={
                 "accuracy": MulticlassAccuracy(),
             },
-            summary_writer=SummaryWriter(log_dir) if log_dir is not None else None,
+            summary_writer=summary_writer,
+            val_summary_writer=val_summary_writer,
             fp16=True,
             max_grad_norm=1.0,
             grad_accumulation_steps=16,
             lr_scheduler=get_linear_lr_scheduler_with_warmup(
                 optimizer,
                 self._warmup_steps,
-                self._epochs * len(train),
+                self._epochs * len(train_data),
             ),
             patience=3 if early_stopping else None,
             checkpoint_path=save_best_path,
@@ -105,143 +119,6 @@ class LongformerIMBD(ExperimentalModel):
             LongformerForSequenceClassification,
             LongformerForSequenceClassification.from_pretrained(dir_path),
         )
-
-    def _train(
-        self,
-        *,
-        train: DataLoader,
-        val: Optional[DataLoader] = None,
-        epochs: int,
-        loss_fn: torch.nn.Module,
-        optimizer: torch.optim.Optimizer,
-        metrics: dict[str, Metric],
-        summary_writer: Optional[SummaryWriter] = None,
-        fp16: bool = False,
-        max_grad_norm: Optional[float] = None,
-        grad_accumulation_steps: int = 1,
-        steps_in_epoch: Optional[int] = None,
-        lr_scheduler=None,
-        progress_bar: bool = True,
-        validate_every: int = 1,
-        checkpoint_path: Optional[str] = None,
-        patience: Optional[int] = None,
-    ) -> None:
-        model = self._model
-        device = model.device
-        min_val_loss = float("inf")
-        epochs_without_improvement = 0
-
-        steps_in_epoch = steps_in_epoch if steps_in_epoch is not None else len(train)
-
-        scaler = GradScaler()
-
-        for name, metric in metrics.items():
-            metrics[name] = metric.to(device)
-        loss_metric = Mean(device=device)
-
-        for epoch in tqdm(range(epochs), desc="Epoch", disable=not progress_bar):
-            for i, batch in tqdm(
-                enumerate(train),
-                desc="Iteration",
-                disable=not progress_bar,
-                total=steps_in_epoch,
-            ):
-                batch_to_device(batch, device)
-
-                with torch.autocast(
-                    device_type=device.type, dtype=torch.float16, enabled=fp16
-                ):
-                    outputs = model(
-                        input_ids=batch["input_ids"],
-                        attention_mask=batch["attention_mask"],
-                    )
-                    loss = (
-                        loss_fn(outputs["logits"], batch["labels"])
-                        / grad_accumulation_steps
-                    )
-
-                    loss_metric.update(loss * grad_accumulation_steps)
-
-                for metric in metrics.values():
-                    metric.update(outputs["logits"], batch["labels"])
-
-                loss = scaler.scale(loss) if fp16 else loss
-                loss.backward()
-
-                if (i + 1) % grad_accumulation_steps == 0 or (i + 1) == steps_in_epoch:
-                    if max_grad_norm is not None:
-                        scaler.unscale_(optimizer)
-                        torch.nn.utils.clip_grad_norm_(
-                            model.parameters(), max_grad_norm
-                        )
-
-                    optimizer_was_run = True
-                    if fp16:
-                        scale_before = scaler.get_scale()
-                        scaler.step(optimizer)
-                        scale_after = scaler.get_scale()
-                        optimizer_was_run = scale_before <= scale_after
-
-                        scaler.update()
-                    else:
-                        optimizer.step()
-
-                    if optimizer_was_run and lr_scheduler is not None:
-                        lr_scheduler.step()
-
-                    # Or model.zero_grad()?
-                    optimizer.zero_grad()
-
-            if summary_writer is not None:
-                summary_writer.add_scalar("loss", loss_metric.compute(), epoch)
-                loss_metric.reset()
-                for name, metric in metrics.items():
-                    summary_writer.add_scalar(name, metric.compute(), epoch)
-                    metric.reset()
-
-            if val is not None and (1 + epoch) % validate_every == 0:
-                model.eval()
-
-                for batch in val:
-                    batch_to_device(batch, device)
-
-                    with torch.autocast(
-                        device_type=device.type, dtype=torch.float16, enabled=fp16
-                    ):
-                        with torch.no_grad():
-                            outputs = model(
-                                input_ids=batch["input_ids"],
-                                attention_mask=batch["attention_mask"],
-                            )
-                            loss = loss_fn(outputs["logits"], batch["labels"])
-                    loss_metric.update(loss * grad_accumulation_steps)
-
-                    for metric in metrics.values():
-                        metric.update(outputs["logits"], batch["labels"])
-
-                val_loss = loss_metric.compute()
-                if val_loss < min_val_loss:
-                    if checkpoint_path is not None:
-                        model.save_pretrained(checkpoint_path)
-                        min_val_loss = val_loss
-
-                    epochs_without_improvement = 0
-                else:
-                    epochs_without_improvement += 1
-
-                if patience is not None and epochs_without_improvement == patience:
-                    break
-
-                if summary_writer is not None:
-                    summary_writer.add_scalar("val_loss", val_loss, epoch)
-                    loss_metric.reset()
-                    for name, metric in metrics.items():
-                        summary_writer.add_scalar(
-                            f"val_{name}", metric.compute(), epoch
-                        )
-                        metric.reset()
-
-                model.train()
 
     def _prepare_data(self, data: IMDBData, training: bool = True) -> DataLoader:
         def _tokenize(doc: dict[str, Any]) -> dict[str, Any]:
@@ -280,3 +157,187 @@ def get_linear_lr_scheduler_with_warmup(
         )
 
     return LambdaLR(optimizer, lr_lambda)
+
+
+def training_step(
+    *,
+    model: torch.nn.Module,
+    batch: dict[str, torch.Tensor],
+    metrics: dict[str, Metric],
+    loss_metric: Metric,
+    fp16: bool = False,
+    grad_accumulation_steps: int = 1,
+    optimizer: torch.optim.Optimizer,
+    loss_fn: torch.nn.Module,
+    scaler: torch.cuda.amp.grad_scaler.GradScaler,
+    steps_in_epoch: int,
+    lr_scheduler: Optional[object] = None,
+    max_grad_norm: Optional[float] = None,
+    step: int,
+) -> None:
+    assert (
+        scaler is not None or not fp16
+    ), f"{training_step.__name__}: scaler must be set for fp16."
+
+    device = torch.device("cuda")
+    batch_to_device(batch, device)
+
+    with torch.autocast(device_type=device.type, dtype=torch.float16, enabled=fp16):
+        outputs = model(
+            input_ids=batch["input_ids"],
+            attention_mask=batch["attention_mask"],
+        )
+        loss = loss_fn(outputs["logits"], batch["labels"]) / grad_accumulation_steps
+
+    loss_metric.update(loss * grad_accumulation_steps)
+    for metric in metrics.values():
+        metric.update(outputs["logits"], batch["labels"])
+
+    loss = scaler.scale(loss) if fp16 else loss
+    loss.backward()
+
+    if (step + 1) % grad_accumulation_steps == 0 or (step + 1) == steps_in_epoch:
+        if max_grad_norm is not None:
+            scaler.unscale_(optimizer)
+            torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+
+        optimizer_was_run = True
+        if fp16:
+            scale_before = scaler.get_scale()
+            scaler.step(optimizer)
+            scale_after = scaler.get_scale()
+            optimizer_was_run = scale_before <= scale_after
+
+            scaler.update()
+        else:
+            optimizer.step()
+
+        if optimizer_was_run and lr_scheduler is not None:
+            lr_scheduler.step()
+
+        # Or model.zero_grad()?
+        optimizer.zero_grad()
+
+
+def log(
+    *,
+    metrics: dict[str, Metric],
+    summary_writer: SummaryWriter,
+    step: int,
+    loss_metric: Metric,
+) -> None:
+    summary_writer.add_scalar("loss", loss_metric.compute(), step)
+    loss_metric.reset()
+    for name, metric in metrics.items():
+        summary_writer.add_scalar(name, metric.compute(), step)
+        metric.reset()
+
+
+def train(
+    *,
+    model: PreTrainedModel,
+    train_data: DataLoader,
+    val_data: Optional[DataLoader] = None,
+    epochs: int,
+    loss_fn: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    metrics: dict[str, Metric],
+    summary_writer: Optional[SummaryWriter] = None,
+    val_summary_writer: Optional[SummaryWriter] = None,
+    fp16: bool = False,
+    max_grad_norm: Optional[float] = None,
+    grad_accumulation_steps: int = 1,
+    steps_in_epoch: Optional[int] = None,
+    lr_scheduler=None,
+    progress_bar: bool = True,
+    validate_every: int = 1,
+    checkpoint_path: Optional[str] = None,
+    patience: Optional[int] = None,
+) -> None:
+    device = torch.device("cuda")
+    model.to(device)
+    min_val_loss = float("inf")
+    epochs_without_improvement = 0
+
+    steps_in_epoch = steps_in_epoch if steps_in_epoch is not None else len(train_data)
+
+    scaler = GradScaler()
+
+    for name, metric in metrics.items():
+        metrics[name] = metric.to(device)
+    loss_metric = Mean(device=device)
+
+    for epoch in tqdm(range(epochs), desc="Epoch", disable=not progress_bar):
+        for i, batch in tqdm(
+            enumerate(train_data),
+            desc="Iteration",
+            disable=not progress_bar,
+            total=steps_in_epoch,
+        ):
+            training_step(
+                model=model,
+                optimizer=optimizer,
+                batch=batch,
+                metrics=metrics,
+                loss_metric=loss_metric,
+                fp16=fp16,
+                grad_accumulation_steps=grad_accumulation_steps,
+                loss_fn=loss_fn,
+                scaler=scaler,
+                steps_in_epoch=steps_in_epoch,
+                step=i,
+                max_grad_norm=max_grad_norm,
+                lr_scheduler=lr_scheduler,
+            )
+
+        if summary_writer is not None:
+            log(
+                metrics=metrics,
+                summary_writer=summary_writer,
+                step=epoch * steps_in_epoch,
+                loss_metric=loss_metric,
+            )
+
+        if val_data is not None and (1 + epoch) % validate_every == 0:
+            model.eval()
+            mean_val_loss = Mean(device=device)
+
+            for batch in val_data:
+                batch_to_device(batch, device)
+
+                with torch.autocast(
+                    device_type=device.type, dtype=torch.float16, enabled=fp16
+                ):
+                    with torch.no_grad():
+                        outputs = model(
+                            input_ids=batch["input_ids"],
+                            attention_mask=batch["attention_mask"],
+                        )
+                        loss = loss_fn(outputs["logits"], batch["labels"])
+
+                mean_val_loss.update(loss)
+                for metric in metrics.values():
+                    metric.update(outputs["logits"], batch["labels"])
+
+            if val_summary_writer is not None:
+                log(
+                    metrics=metrics,
+                    summary_writer=val_summary_writer,
+                    step=epoch * steps_in_epoch,
+                    loss_metric=mean_val_loss,
+                )
+
+            val_loss = mean_val_loss.compute()
+            if val_loss < min_val_loss:
+                if checkpoint_path is not None:
+                    model.save_pretrained(checkpoint_path)
+                    min_val_loss = val_loss
+
+                epochs_without_improvement = 0
+            else:
+                epochs_without_improvement += 1
+
+            if patience is not None and epochs_without_improvement == patience:
+                break
+
+            model.train()
