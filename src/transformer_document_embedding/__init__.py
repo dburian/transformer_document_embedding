644f756863ae766df34bab7e8b1be841153ff083
David Burian <david.burian@me.com>
2023-04-12 15:40:06 +0200

    Fix to get runnable code for AIC.

--- src/transformer_document_embedding/__init__.py
+++ src/transformer_document_embedding/__init__.py
@@ -1,6 +1,10 @@
 import os
 
+# Importing torch after tensorflow causes wierd stuff to happen on AIC
+# TODO: File an issue
+import torch
+
 # Report only TF errors by default
 os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")
 
-from . import experiments, models, tasks, baselines
+from . import baselines, experiments, models, tasks
