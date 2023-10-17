c3292fb7528de0d14e9f6c195d567ec88c81090a
David Burian <david.burian@me.com>
2023-10-17 12:58:45 +0200

    Add BookCorpus task

---
+++ src/transformer_document_embedding/tasks/bookcorpus.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from datasets import DatasetDict, load_dataset
+from transformer_document_embedding.tasks.hf_task import HFTask
+
+if TYPE_CHECKING:
+    from typing import Optional
+
+
+class BookCorpus(HFTask):
+    def __init__(
+        self,
+        *,
+        data_size_limit: Optional[int] = None,
+        validation_source_fraction: Optional[float] = None,
+        validation_source: Optional[str] = None,
+    ) -> None:
+        super().__init__(
+            data_size_limit=data_size_limit,
+            add_ids=True,
+            validation_source_fraction=validation_source_fraction,
+            validation_source=validation_source,
+        )
+
+    def _retrieve_dataset(self) -> DatasetDict:
+        ds = load_dataset("bookcorpus")
+        assert isinstance(ds, DatasetDict)
+        return ds
