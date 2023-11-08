6aee35385226d6b9740df538220d110b899fd7fb
David Burian <david.burian@me.com>
2023-11-08 17:53:05 +0100

    notebooks: move from jupyter ascending to jupytext inside jupter lab
    - add c4 loading notebook
    - remove notebooks that are not needed anymore
    - ruff: ignore rules that are not sensible in notebooks

--- notebooks/sbert.nb.py
+++ notebooks/sbert.nb.py
@@ -5,12 +5,13 @@
 #       extension: .py
 #       format_name: percent
 #       format_version: '1.3'
-#       jupytext_version: 1.3.4
+#       jupytext_version: 1.15.2
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
+
 # %% [markdown]
 # # Sentence transformers
 # %%
