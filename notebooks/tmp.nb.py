b02b7408431f78d46039394274365512fa74da9a
David Burian <david.burian@me.com>
2023-11-14 10:07:36 +0100

    notebooks: cleanup tmp.nb.py

--- notebooks/tmp.nb.py
+++ notebooks/tmp.nb.py
@@ -1,88 +1,24 @@
 # %%
-from transformer_document_embedding.baselines.longformer.student import (
-    LongformerStudent,
-)
-from transformer_document_embedding.tasks.teacher_embedding import TeacherEmbedding
-import transformer_document_embedding.utils.torch.training as train_utils
-from transformers import AutoTokenizer
+from transformer_document_embedding.experiments.config import ExperimentConfig
 
 # %%
-model = LongformerStudent(
-    large=False,
-    batch_size=2,
-    pooler_type="mean",
-    static_embed_dim=100,
-    contextual_max_length=384,
-    static_contextual_lambda=0.5,
-    static_loss_type=True,
-    cca_output_dim=50,
-    projection_norm="layer",
-    longformer_projection_layers=[256],
-    static_projection_layers=[64],
+exp_path = (
+    "../results/teacher_embedding:TeacherEmbedding/"
+    "longformer.student:LongformerStudent/private_galago"
 )
 
 # %%
-print(
-    "\n".join(
-        [f"{name}: {param.shape}" for name, param in model._model.named_parameters()]
-    )
-)
+config = ExperimentConfig.from_yaml(exp_path + "/config.yaml", "../results")
 
 # %%
-print(model._model.get_parameter("loss._static_loss._net2.layers.2.weight").grad)
-
-# %%
-task = TeacherEmbedding("/mnt/data/datasets/wikipedia_sample")
-
-# %%
-train_data = train_utils.create_tokenized_data_loader(
-    task.train,
-    tokenizer=AutoTokenizer.from_pretrained("allenai/longformer-base-4096"),
-    batch_size=2,
-    sampling="consistent",
-    training=True,
-    return_length=False,
-    sampler_kwargs={
-        "effective_batch_size": 2 * 10,
-        "bucket_limits": [384],
-        # "short_count": short_inputs_in_effective_batch,
-        # "short_threshold": self._contextual_max_length,
-    },
-)
+model = config.get_model_type()(**config.values["model"]["kwargs"])
 
 
 # %%
-for batch in train_data:  # noqa: B007
-    break
+model.save("./longformer_save")
 
 # %%
-model._model.train()
-
-# %%
-loss = model._model(**batch)
-loss
-
-# %%
-loss["loss"].backward()
-
-# %%
-loss.keys()
-
-# %%
-loss["static_loss"]
-
-# %%
-loss["contextual_loss"]
-
-# %%
-loss["loss"]
-
-# %%
-loss["static_loss"] + loss["contextual_loss"] * 0.5
-
-# %%
-param = "loss._static_loss._net2.layers.2.weight"
-model._model.get_parameter(param).grad
+model.load(exp_path + "/model/checkpoint")
 
 # %% [markdown]
 # ----
@@ -122,38 +58,7 @@ make_dot(
 
 # %% [markdown]
 # ---
-# ## Testing detach
-
-# %%
-a = torch.tensor([1, 2, 3, 4], requires_grad=True, dtype=torch.float)
-b = torch.tensor([1, 2, 3, 4], requires_grad=True, dtype=torch.float)
-
-# %%
-a = a.detach()
-a = a + b
-
-# %%
-a
-
-# %%
-loss = torch.sum(a)
-
-# %%
-loss.backward()
-
-# %%
-make_dot(loss, params={"a": a, "b": b}).render("detach", format="pdf")
-
-# %%
-param = "transformer.longformer.encoder.layer.11.output.LayerNorm.weight"
-print(model._model.get_parameter(param).grad)
-
-# %%
-from torchviz import make_dot
-
-make_dot(loss["loss"], params=dict(list(model._model.named_parameters()))).render(
-    "rnn_torchviz", format="pdf"
-)
+# ## Creating true wikipedia dbow correlation matrix
 
 # %%
 from datasets import load_from_disk
