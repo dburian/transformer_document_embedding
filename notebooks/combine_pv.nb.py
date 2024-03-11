794906e2d37f6d09b2d7e20f97a37d4cf4707a55
David Burian <david.burian@me.com>
2024-03-11 18:05:24 +0100

    notebooks: fix combine PV notebook

--- notebooks/combine_pv.nb.py
+++ notebooks/combine_pv.nb.py
@@ -21,14 +21,16 @@ DBOWs = [""]
 
 
 # %%
-def load_config(model_save_path: str) -> ExperimentSpec:
+def load_config(model_save_path: str) -> tuple[ExperimentSpec, str]:
     config_path = find_config(model_save_path)
     if config_path is None:
         raise ValueError(
             f"Cannot find config file for model saved at '{model_save_path}'."
         )
 
-    return ExperimentSpec.from_dict(**load_yaml(config_path))
+    return ExperimentSpec.from_dict(load_yaml(config_path)), os.path.dirname(
+        config_path
+    )
 
 
 # %% [markdown]
@@ -39,7 +41,8 @@ from itertools import product
 
 # %%
 for dm_path, dbow_path in product(DMs, DBOWs):
-    dm_config, dbow_config = load_config(dm_path), load_config(dbow_path)
+    dm_config, dm_dir = load_config(dm_path)
+    dbow_config, dbow_dir = load_config(dbow_path)
 
     combined_config = ExperimentSpec(
         model=EmbeddingModelSpec(
@@ -54,15 +57,19 @@ for dm_path, dbow_path in product(DMs, DBOWs):
         train_pipeline=None,
     )
 
-    dm_name = os.path.basename(dm_path)
-    dbow_name = os.path.basename(dbow_path)
+    dm_name = os.path.basename(dm_dir)
+    dbow_name = os.path.basename(dbow_dir)
     exp_path = os.path.join(
         "results",
         combined_config.dataset.module,
         combined_config.model.module,
         f"dm={dm_name}-dbow={dbow_name}",
     )
+    os.makedirs(exp_path, exist_ok=True)
     save_config(combined_config, exp_path)
+
+    model_weights_dir = os.path.join(exp_path, "trained_model")
+    os.makedirs(model_weights_dir, exist_ok=True)
 
     for original_path, target_name in zip(
         [dm_path, dbow_path], ["dm", "dbow"], strict=True
@@ -79,7 +86,7 @@ for dm_path, dbow_path in product(DMs, DBOWs):
             target_filename = re.sub(original_name, target_name, filename)
             shutil.copy(
                 os.path.join(original_dir, filename),
-                os.path.join(exp_path, target_filename),
+                os.path.join(model_weights_dir, target_filename),
             )
     print(f"Created model from dm={dm_name} and dbow={dbow_name} at '{exp_path}'")
 # %%
