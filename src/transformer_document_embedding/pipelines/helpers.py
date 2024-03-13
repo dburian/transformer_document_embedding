a37877839ed046faca86624429efbc9ea32959bc
David Burian <david.burian@me.com>
2024-03-13 13:50:36 +0100

    pipelines: add macro-averaged classification metrics

--- src/transformer_document_embedding/pipelines/helpers.py
+++ src/transformer_document_embedding/pipelines/helpers.py
@@ -10,21 +10,30 @@ from torcheval.metrics import (
 
 
 def classification_metrics(num_classes: int, **metric_kwargs) -> dict[str, Metric]:
-    accuracy = MulticlassAccuracy(
+    micro_accuracy = MulticlassAccuracy(
         num_classes=num_classes, average="micro", **metric_kwargs
     )
 
-    recall = MulticlassRecall(num_classes=num_classes, average="micro", **metric_kwargs)
-
-    precision = MulticlassPrecision(
-        num_classes=num_classes, average="micro", **metric_kwargs
+    macro_accuracy = MulticlassAccuracy(
+        num_classes=num_classes, average="macro", **metric_kwargs
     )
 
-    f1 = MulticlassF1Score(num_classes=num_classes, average="micro", **metric_kwargs)
+    macro_recall = MulticlassRecall(
+        num_classes=num_classes, average="macro", **metric_kwargs
+    )
+
+    macro_precision = MulticlassPrecision(
+        num_classes=num_classes, average="macro", **metric_kwargs
+    )
+
+    macro_f1 = MulticlassF1Score(
+        num_classes=num_classes, average="macro", **metric_kwargs
+    )
 
     return {
-        "precision": precision,
-        "accuracy": accuracy,
-        "f1": f1,
-        "recall": recall,
+        "micro_accuracy": micro_accuracy,
+        "macro_accuracy": macro_accuracy,
+        "macro_precision": macro_precision,
+        "macro_f1": macro_f1,
+        "macro_recall": macro_recall,
     }
