b8f6fdc47db1eabf511ecc61c8f17b10a963b582
David Burian <david.burian@me.com>
2024-03-25 07:50:50 +0100

    scripts: `generate_embeddings` to behave like `evaluate`

--- src/transformer_document_embedding/scripts/generate_embeddings.py
+++ src/transformer_document_embedding/scripts/generate_embeddings.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from copy import deepcopy
 
 
 import os
@@ -10,8 +11,7 @@ import pprint
 from datasets import Dataset, DatasetDict, concatenate_datasets
 import datasets.utils.logging as hf_logging
 from transformer_document_embedding.datasets import col
-from transformer_document_embedding.scripts.common import load_train_save
-from transformer_document_embedding.scripts.config_specs import ExperimentSpec
+from transformer_document_embedding.scripts.evaluate import find_config
 from transformer_document_embedding.scripts.utils import (
     load_yaml,
     save_config,
@@ -20,6 +20,10 @@ from transformer_document_embedding.scripts.utils import (
 import coolname
 
 from transformer_document_embedding.pipelines.classification_eval import smart_unbatch
+from transformer_document_embedding.scripts.config_specs import ExperimentSpec
+
+
+logger = logging.getLogger(__name__)
 
 
 def parse_args() -> argparse.Namespace:
@@ -43,41 +47,20 @@ def parse_args() -> argparse.Namespace:
     )
 
     parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        action="extend",
+        nargs="+",
+        help="Path to model's weights.",
+    )
+
+    parser.add_argument(
         "-n",
         "--name",
         type=str,
         default="_".join(coolname.generate(2)),
         help="Name of the experiment. If no name is given, one is generated.",
-    )
-
-    parser.add_argument(
-        "--save_trained_model",
-        type=bool,
-        action=argparse.BooleanOptionalAction,
-        # default=self._save_trained,
-        help="Whether to save trained model.",
-    )
-
-    parser.add_argument(
-        "--save_trained_head",
-        type=bool,
-        action=argparse.BooleanOptionalAction,
-        # default=self._save_trained,
-        help="Whether to save trained head.",
-    )
-
-    parser.add_argument(
-        "--load_model_weights_path",
-        type=str,
-        default=None,
-        help="Path from where to load model's weights.",
-    )
-
-    parser.add_argument(
-        "--load_head_weights_path",
-        type=str,
-        default=None,
-        help="Path from where to load head's weights.",
     )
 
     parser.add_argument(
@@ -96,39 +79,30 @@ def parse_args() -> argparse.Namespace:
         "the dataset.",
     )
 
-    parser.add_argument(
-        "--max_shard_size",
-        type=str,
-        default="1GB",
-        help="Maximum size of generated shards.",
-    )
-
     return parser.parse_args()
 
 
 def generate_embeddings(
     config: ExperimentSpec,
+    model_weights_path: str,
     args: argparse.Namespace,
 ) -> None:
     exp_path = os.path.join(
         args.output_base_path, config.dataset.module, config.model.module, args.name
     )
     os.makedirs(exp_path, exist_ok=True)
-    logging.info(
+    logger.info(
         "Generating embeddings '%s' with config:\n%s",
         args.name,
         pprint.pformat(config, indent=1),
     )
     save_config(config, exp_path)
 
-    model, _, dataset = load_train_save(
-        config,
-        save_trained_model=args.save_trained_model,
-        save_trained_head=args.save_trained_head,
-        load_model_weights_path=args.load_model_weights_path,
-        load_head_weights_path=args.load_head_weights_path,
-        exp_path=exp_path,
-    )
+    dataset = config.dataset.initialize()
+    model = config.model.initialize()
+
+    logger.info("Loading model weights from '%s'.", model_weights_path)
+    model.load_weights(model_weights_path)
 
     # The gymnastics with generators, new dataset and concatenation is not
     # straightforward, but:
@@ -149,10 +123,10 @@ def generate_embeddings(
 
     hf_logging.disable_progress_bar()
     for split_name in splits:
-        logging.info("Generating embeddings for split '%s'", split_name)
+        logger.info("Generating embeddings for split '%s'", split_name)
         split = dataset.splits.get(split_name, None)
         if split is None:
-            logging.warn(
+            logger.warn(
                 "Split '%s' doesn't exist for this dataset. Skipping...", split_name
             )
             continue
@@ -164,8 +138,8 @@ def generate_embeddings(
     hf_logging.enable_progress_bar()
 
     embed_dataset_path = os.path.join(exp_path, "embeddings")
-    logging.info("Saving the embeddings dataset to '%s'", embed_dataset_path)
-    embeddings.save_to_disk(embed_dataset_path, max_shard_size=args.max_shard_size)
+    logger.info("Saving the embeddings dataset to '%s'", embed_dataset_path)
+    embeddings.save_to_disk(embed_dataset_path)
 
 
 def main():
@@ -175,8 +149,23 @@ def main():
         format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO
     )
 
-    config = ExperimentSpec.from_dict(load_yaml(args.config))
-    generate_embeddings(config, args)
+    config = load_yaml(args.config)
+    config.pop("head", None)
+    config.pop("train_pipeline", None)
+    for model_weights_path in args.model:
+        model_conf_path = find_config(model_weights_path)
+        if model_conf_path is None:
+            logger.error(
+                "Cannot find config for a model's weights path '%s'", model_weights_path
+            )
+            continue
+        model_conf = load_yaml(model_conf_path)
+        instance_config = deepcopy(config)
+        instance_config["model"] = model_conf["model"]
+
+        generate_embeddings(
+            ExperimentSpec.from_dict(instance_config), model_weights_path, args
+        )
 
 
 if __name__ == "__main__":
