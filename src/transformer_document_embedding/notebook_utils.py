7c5455a960b976d9e376285ee68082baf037d0ce
David Burian <david.burian@me.com>
2024-05-02 15:44:44 +0200

    notebook_utils: updates

--- src/transformer_document_embedding/notebook_utils.py
+++ src/transformer_document_embedding/notebook_utils.py
@@ -1,10 +1,30 @@
 from __future__ import annotations
 
 import os
-from typing import Any, Callable, Optional
+from typing import Any, Callable, Iterable, Optional, TYPE_CHECKING
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from transformer_document_embedding.scripts.config_specs import EmbeddingModelSpec
+from transformer_document_embedding.scripts.evaluate import find_config
 
 from transformer_document_embedding.scripts.utils import load_yaml
+import logging
+
+if TYPE_CHECKING:
+    from transformer_document_embedding.models.embedding_model import EmbeddingModel
+
+logger = logging.getLogger(__name__)
+
+boxplot_kwargs = dict(
+    showmeans=True,
+    meanprops={
+        "markeredgecolor": "black",
+        "markerfacecolor": "black",
+        "marker": "^",
+        "markeredgewidth": 1,
+    },
+)
 
 
 def simplify_metric_name(metric_name: str) -> str:
@@ -18,11 +38,16 @@ def simplify_metric_name(metric_name: str) -> str:
 
 def load_validation_results(
     eval_dir: str,
-    model_features: Optional[Callable[[str], dict[str, Any]]],
+    model_features: Optional[Callable[[str], dict[str, Any]]] = None,
 ) -> pd.DataFrame:
     model_evals = []
     for model_dir in os.scandir(eval_dir):
-        model_eval_results = load_yaml(os.path.join(model_dir.path, "results.yaml"))
+        try:
+            model_eval_results = load_yaml(os.path.join(model_dir.path, "results.yaml"))
+        except FileNotFoundError:
+            logger.warn("Results for model '%s' not found. Skipping...", model_dir.name)
+            continue
+
         for task, metrics in model_eval_results.items():
             for metric_name, score in metrics.items():
                 if metric_name.endswith("_std"):
@@ -70,3 +95,35 @@ def add_normalized_score(
 
     by_metric["normalized_score"] = by_metric["score"] / metric_maximums
     return by_metric.reset_index()
+
+
+def seaborn_defaults() -> None:
+    plt.rc("figure", figsize=(15, 8))
+    sns.set_theme("paper", "whitegrid", "colorblind")
+
+
+def uncompressed_tables() -> None:
+    pd.set_option("display.max_columns", 1000)
+    pd.set_option("display.max_rows", 1000)
+    pd.set_option("display.max_colwidth", 500)
+
+
+def model_order(
+    evals: pd.DataFrame, by: str = "score", of: str = "model"
+) -> Iterable[str]:
+    return evals.groupby(of, observed=False)[by].mean().sort_values().index
+
+
+def load_model_save(path_to_weights: str) -> EmbeddingModel:
+    config_path = find_config(path_to_weights)
+    if config_path is None:
+        raise ValueError(
+            "Cannot find `config.yaml` file for model saved at '%s'. Skipping..."
+            % path_to_weights,
+        )
+
+    model_config = EmbeddingModelSpec.from_dict(load_yaml(config_path)["model"])
+
+    model = model_config.initialize()
+    model.load_weights(path_to_weights)
+    return model
